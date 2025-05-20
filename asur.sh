8cf59d867a62345601d83857714b02d028045883
deemahts <mdeemanth2006@gmail.com>
2025-05-20 22:17:44 +0530

    Update asur.sh

--- asur.sh
+++ asur.sh
@@ -12,7 +12,7 @@ REQUIRED_PACKAGES=("git" "build-essential" "cmake" "automake" "libtool" "autocon
 # Function to check and install missing dependencies
 install_dependencies() {
     for package in "${REQUIRED_PACKAGES[@]}"; do
-        dpkg -l | grep -qw $package || sudo apt install -y $package
+        sudo apt install -y $package
     done
 }
 
